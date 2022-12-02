344b8084c82681284709e11c180e95c3b0caa7e1
Arek <ogryzek.arkadiusz@gmail.com>
2022-12-02 23:20:40 +0100

    .

--- Basics/basics.py
+++ Basics/basics.py
@@ -78,3 +78,56 @@ def myFunc2():
 
 myFunc2()  # Global keyword to change global variables
 print(x)
+
+
+# Data types
+
+x1 = "string" # string
+x2 = 5 # int
+x3 = 5.5 # float
+x4 = 5j # complex
+
+# sequence
+x5 = [1,2,3] # list
+x6 = (1,2,3) # tuple
+x7 = range(3) # range
+
+# mapping
+x8 = {"name":"dict"}
+
+x9 = {"set","whatever","it","is"}
+x10 = frozenset({"another",1})
+
+x11 = True
+
+# ? binary ?
+x12 = b"bytes here"
+print(x12) # string but type bytes
+x13 = bytearray(10)
+print(x13) # this thing
+x14 = memoryview(bytes(5))
+print(x14) # another this thing
+
+# Just none
+x15 = None
+
+x = str("Hello")
+x = int(20)
+x = float(5.5)
+x = complex(2j)
+# Just iterable inside
+x = list(("one","two"))
+x = tuple(("one","two"))
+x = range(6)
+for i in x:
+    print(i)
+x = dict(name="John", surname="Doe")
+x = set(("ite","rable"))
+x = frozenset(("frozen","can't change"))
+x = bool(1)
+
+# ?
+x = bytes(5)
+x = bytearray(5)
+x = memoryview(bytes(5))
+# ?
