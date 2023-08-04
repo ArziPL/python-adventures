24d19f0fc2826b065ac4dccdb81a8add1928724d
ArziPL <ogryzek.arkadiusz@gmail.com>
2023-08-04 12:46:15 +0200

    Band name generator

--- band_name_generator.py
+++ band_name_generator.py
@@ -1,5 +1,5 @@
 # Day 1 - project 1
 print("Band name generator")
-city = input("What's your city name? ")
-pet = input("What's your pet name? ")
+city = input("What's your city name?\n")
+pet = input("What's your pet name?\n")
 print("Your band name could be: " + pet + " " + city)
