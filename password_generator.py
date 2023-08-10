0287ab05bca1b853c420f5791845b1cb00230cec
ArziPL <ogryzek.arkadiusz@gmail.com>
2023-08-10 12:37:41 +0200

    Password generator

---
+++ password_generator.py
@@ -0,0 +1,32 @@
+# Day 5 - project 2
+import string
+import random
+
+# User input + tests
+pass_len = int(input("How many letters would you like in your password? "))
+pass_symbols = int(input(f"How many symbols would you like? Maximum available: {pass_len} => "))
+while pass_symbols > pass_len:
+    pass_symbols = int(input(f"Please, write the correct number for amount of symbols. Maximum available: {pass_len} => "))
+pass_numbers = int(input(f"How many numbers would you like? Maximum available: {pass_len - pass_symbols} => "))
+while pass_numbers > pass_len - pass_symbols:
+    pass_numbers = int(input(f"Please, write the correct number for amount of numbers. Maximum available: {pass_len - pass_symbols} => "))
+
+# Generating random char password
+password = [None] * (pass_len)
+for i in range(1,pass_len + 1):
+    password[i - 1] = random.choice(string.ascii_letters)
+
+# Adding random symbols and numbers
+while pass_symbols > 0 or pass_numbers > 0:
+    current_position = random.randint(0,pass_len - 1)
+    if(password[current_position] in string.ascii_letters):
+        if pass_symbols > 0:
+            password[current_position] = random.choice(string.punctuation)
+            pass_symbols = pass_symbols - 1
+        else:
+            password[current_position] = random.choice(string.digits)
+            pass_numbers = pass_numbers - 1
+
+print(f"Your password is {''.join(password)}") 
+
+
